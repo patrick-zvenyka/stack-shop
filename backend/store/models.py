df527f65ce9704eff529a7167082a2045ab173f7
patrick-zvenyka <patrickzvenyika1999@gmail.com>
2026-02-07 20:42:33 +0200

    model-fix

--- backend/store/models.py
+++ backend/store/models.py
@@ -12,6 +12,7 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+# Product
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
